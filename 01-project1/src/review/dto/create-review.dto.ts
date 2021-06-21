e21fd436cbfe7102db5d243722d2f60c7b7b1269
Roma Makarov <r0mmm4k@gmail.com>
2021-06-21 18:22:12 +0300

    add review validation pipe

--- 01-project1/src/review/dto/create-review.dto.ts
+++ 01-project1/src/review/dto/create-review.dto.ts
@@ -1,7 +1,20 @@
+import { IsNumber, IsString, Max, Min } from 'class-validator';
+
 export class CreateReviewDto {
+  @IsString()
   name: string;
+
+  @IsString()
   title: string;
+
+  @IsString()
   description: string;
+
+  @IsNumber()
+  @Min(1, { message: 'Рейтинг не может быть менее 1' })
+  @Max(5)
   rating: number;
+
+  @IsString()
   productId: string;
 }
