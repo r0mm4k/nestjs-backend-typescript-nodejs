f625fe7db41eb26c659ac6b887697e64b2433693
Roma Makarov <r0mmm4k@gmail.com>
2021-06-24 13:05:37 +0300

    fix review delete tests with auth

--- test/review.e2e-spec.ts
+++ test/review.e2e-spec.ts
@@ -6,6 +6,7 @@ import { disconnect, Types } from 'mongoose';
 import { AppModule } from '../src/app.module';
 import { CreateReviewDto } from '../src/review/dto/create-review.dto';
 import { REVIEW_NOT_FOUND } from '../src/review/review.constants';
+import { AuthDto } from '../src/auth/dto/auth.dto';
 
 const productId = new Types.ObjectId().toHexString();
 
@@ -17,9 +18,15 @@ const testDto: CreateReviewDto = {
   productId,
 };
 
+const loginDto: AuthDto = {
+  login: 'test@test.com',
+  password: '12345',
+};
+
 describe('AppController (e2e)', () => {
   let app: INestApplication;
   let createdId: string;
+  let token: string;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -28,6 +35,12 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    const { body } = await request(app.getHttpServer())
+      .post('/auth/login')
+      .send(loginDto);
+
+    token = body.accessToken;
   });
 
   it('/review/create (POST) - success', async (done) => {
@@ -72,12 +85,14 @@ describe('AppController (e2e)', () => {
   it('/review/:id (DELETE) - success', () => {
     return request(app.getHttpServer())
       .delete('/review/' + createdId)
+      .set('Authorization', `Bearer ${token}`)
       .expect(200);
   });
 
   it('/review/:id (DELETE) - fail', () => {
     return request(app.getHttpServer())
       .delete('/review/' + new Types.ObjectId().toHexString())
+      .set('Authorization', `Bearer ${token}`)
       .expect(404, {
         statusCode: 404,
         message: REVIEW_NOT_FOUND,
